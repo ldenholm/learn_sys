d3725d0ef4bb6e03bbca1e026fbe94ed6bf3fd11
ldenholm <disrute@gmail.com>
2025-06-05 16:43:13 +1000

    upd write

--- cs631/fd_warmup/file_descriptors.c
+++ cs631/fd_warmup/file_descriptors.c
@@ -1,6 +1,8 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include <unistd.h>
+#include <fcntl.h>
+#include <string.h>
 
 int main(int argc, char** argv) {
 	//char* path = argv[1];
@@ -16,5 +18,25 @@ int main(int argc, char** argv) {
 	printf("stdin fd = %d \n", fd);
 	printf("stdout fd = %d \n", fdout);
 	printf("stderr fd = %d \n", fderr);
+	
+	int open_one, open_two;
+	char msg[] = "Flibbidy Biscuit";
+	ssize_t written;
+
+	// open file using open():
+	open_one = open("/home/lochie/Git/learn_sys/cs631/fd_warmup/tmpFile.txt", O_RDWR);
+	printf("open_one fd = %d", open_one);
+	// issue a write here
+	write(open_one, msg, strlen(msg));	
+
+
+	open_two = open("/home/lochie/Git/learn_sys/cs631/fd_warmup/tmpFile.txt", O_RDWR);
+	printf("open_two fd = %d", open_two);
+	// second write here, without first closing:
+	write(open_two, msg, strlen(msg));	
+
+	// open file using fopen(), have not closed first open yet:
+	//FILE* notes;
+       	//notes = fopen("/home/lochie/Git/learn_sys/cs631/fd_warmup/tmpFile.txt", "r");
 	return EXIT_SUCCESS;
 }
